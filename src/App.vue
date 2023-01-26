d5b588d17500ca4c2686afa60ecf378958c55775
Federico Sosa <federicososa@Federicos-MacBook-Air.local>
2023-01-26 20:57:56 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <div></div>
+  <!--
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  -->
 </template>
 
 <style lang="scss">
